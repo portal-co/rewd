4b6d0402d45720683ff15097e4861b5fc12c38ba
Graham Kelly <gkgoat6700@gmail.com>
2025-09-13 16:33:02 +0000

    props and stuff

--- portal-solutions-rewd-core/src/lib.rs
+++ portal-solutions-rewd-core/src/lib.rs
@@ -1,16 +1,28 @@
-use std::mem::take;
+use std::{
+    collections::BTreeSet,
+    mem::{replace, take},
+};
 
 use swc_atoms::Atom;
 use swc_common::{Mark, Span, Spanned, SyntaxContext};
 use swc_ecma_ast::{
-    AssignExpr, AssignOp, CallExpr, Class, ClassMember, ComputedPropName, Decl, ExportDecl, Expr,
-    ExprOrSpread, ExprStmt, Ident, IdentName, Lit, MemberExpr, MethodKind, ModuleDecl, ModuleItem,
-    Number, PrivateName, PrivateProp, Prop, PropName, SeqExpr, Stmt, Str, ThisExpr,
+    ArrayLit, AssignExpr, AssignOp, BinExpr, BinaryOp, CallExpr, Class, ClassMember,
+    ComputedPropName, Decl, ExportDecl, Expr, ExprOrSpread, ExprStmt, Id, Ident, IdentName,
+    KeyValueProp, Lit, MemberExpr, MemberProp, MethodKind, ModuleDecl, ModuleItem, Number,
+    PrivateName, PrivateProp, Prop, PropName, PropOrSpread, SeqExpr, Stmt, Str, ThisExpr, VarDecl,
+    VarDeclarator,
 };
 use swc_ecma_visit::{VisitMut, VisitMutWith};
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Default, Debug)]
+#[non_exhaustive]
+pub struct CommonConfig {
+    pub name: Atom,
+    pub hookable: BTreeSet<Atom>,
+}
 
 pub struct FRewrite {
     pub core: Expr,
+    pub cfg: CommonConfig,
 }
 impl FRewrite {
     fn func(&mut self, a: Expr, span: Span) -> Expr {
@@ -108,7 +120,7 @@ impl FRewrite {
                     let prop = match &mut m.key {
                         swc_ecma_ast::PropName::Computed(computed_prop_name) => {
                             // let id2 = Ident::new_private(Atom::new("key"), c.span);
-                            let name = Atom::new(format!("$key${ix}"));
+                            let name = Atom::new(format!("{}$key${ix}", &self.cfg.name));
                             ix += 1;
                             computed_prop_name.expr = Box::new(Expr::Assign(AssignExpr {
                                 span: c.span,
@@ -193,7 +205,7 @@ impl FRewrite {
                 ctxt: SyntaxContext::empty().apply_mark(Mark::new()),
                 key: PrivateName {
                     span: c.span,
-                    name: Atom::new("$clinit"),
+                    name: Atom::new(format!("{}$clinit", &self.cfg.name)),
                 },
                 type_ann: None,
                 is_static: true,
@@ -215,7 +227,7 @@ impl FRewrite {
                 ctxt: SyntaxContext::empty().apply_mark(Mark::new()),
                 key: PrivateName {
                     span: c.span,
-                    name: Atom::new("$init"),
+                    name: Atom::new(format!("{}$init", &self.cfg.name)),
                 },
                 type_ann: None,
                 is_static: false,
@@ -232,7 +244,7 @@ impl FRewrite {
             }));
         }
         c.body.extend((0..ix).map(|ix| {
-            let name = Atom::new(format!("$key${ix}"));
+            let name = Atom::new(format!("{}$key${ix}", &self.cfg.name));
             ClassMember::PrivateProp(PrivateProp {
                 span: c.span,
                 ctxt: SyntaxContext::empty().apply_mark(Mark::new()),
@@ -253,7 +265,11 @@ impl FRewrite {
 impl VisitMut for FRewrite {
     fn visit_mut_private_name(&mut self, node: &mut PrivateName) {
         node.visit_mut_children_with(self);
-        if node.name.starts_with("$") {
+        if node
+            .name
+            .strip_prefix(&*self.cfg.name)
+            .is_some_and(|a| a.starts_with("$"))
+        {
             node.name = Atom::new(format!("${}", node.name));
         }
     }
@@ -274,7 +290,9 @@ impl VisitMut for FRewrite {
                 let s = c.span();
                 let id = c
                     .ident
-                    .get_or_insert_with(|| Ident::new_private(Atom::new("Class"), s))
+                    .get_or_insert_with(|| {
+                        Ident::new_private(Atom::new(format!("{}$Class", &self.cfg.name)), s)
+                    })
                     .clone();
                 self.class(&mut *c.class, id);
                 Expr::Class(c)
@@ -373,5 +391,255 @@ impl VisitMut for FRewrite {
                 a => vec![a],
             })
             .collect();
+    }
+}
+pub struct CoreRewrite {
+    pub core: Expr,
+    pub cfg: CommonConfig,
+    pub decls: BTreeSet<Id>,
+}
+impl VisitMut for CoreRewrite {
+    fn visit_mut_stmts(&mut self, node: &mut Vec<Stmt>) {
+        let old = take(&mut self.decls);
+        node.visit_mut_children_with(self);
+        let old = replace(&mut self.decls, old);
+        node.insert(
+            0,
+            Stmt::Decl(Decl::Var(Box::new(VarDecl {
+                span: Span::dummy_with_cmt(),
+                ctxt: Default::default(),
+                kind: swc_ecma_ast::VarDeclKind::Let,
+                declare: false,
+                decls: old
+                    .into_iter()
+                    .map(|a| VarDeclarator {
+                        span: Span::dummy_with_cmt(),
+                        init: None,
+                        definite: false,
+                        name: swc_ecma_ast::Pat::Ident(a.into()),
+                    })
+                    .collect(),
+            }))),
+        );
+    }
+    fn visit_mut_module(&mut self, node: &mut swc_ecma_ast::Module) {
+        let old = take(&mut self.decls);
+        node.visit_mut_children_with(self);
+        let old = replace(&mut self.decls, old);
+        node.body.insert(
+            0,
+            ModuleItem::Stmt(Stmt::Decl(Decl::Var(Box::new(VarDecl {
+                span: Span::dummy_with_cmt(),
+                ctxt: Default::default(),
+                kind: swc_ecma_ast::VarDeclKind::Let,
+                declare: false,
+                decls: old
+                    .into_iter()
+                    .map(|a| VarDeclarator {
+                        span: Span::dummy_with_cmt(),
+                        init: None,
+                        definite: false,
+                        name: swc_ecma_ast::Pat::Ident(a.into()),
+                    })
+                    .collect(),
+            })))),
+        );
+    }
+    fn visit_mut_with_stmt(&mut self, node: &mut swc_ecma_ast::WithStmt) {
+        node.visit_mut_children_with(self);
+        node.obj = Box::new(Expr::Call(CallExpr {
+            span: node.span,
+            ctxt: Default::default(),
+            callee: swc_ecma_ast::Callee::Expr(Box::new(Expr::Member(MemberExpr {
+                span: node.span,
+                obj: Box::new(self.core.clone()),
+                prop: swc_ecma_ast::MemberProp::Ident(IdentName {
+                    span: node.span,
+                    sym: Atom::new("$with"),
+                }),
+            }))),
+            args: [
+                ExprOrSpread {
+                    expr: take(&mut node.obj),
+                    spread: None,
+                },
+                ExprOrSpread {
+                    spread: None,
+                    expr: Box::new(Expr::Lit(Lit::Str(Str {
+                        span: node.span,
+                        value: Atom::new(&*self.cfg.name),
+                        raw: None,
+                    }))),
+                },
+            ]
+            .into_iter()
+            .collect(),
+            type_args: None,
+        }))
+    }
+    fn visit_mut_member_expr(&mut self, node: &mut MemberExpr) {
+        node.visit_mut_children_with(self);
+        if let MemberProp::PrivateName(_) = &node.prop {
+            return;
+        }
+        let camobj = Ident::new_private(Atom::new(format!("{}$camobj", &self.cfg.name)), node.span);
+        self.decls.insert(camobj.to_id());
+        // let cammem = Ident::new_private(Atom::new(format!("{}$cammem",&self.cfg.name)), node.span);
+        node.obj = Box::new(Expr::Assign(AssignExpr {
+            span: node.span,
+            op: AssignOp::Assign,
+            left: camobj.clone().into(),
+            right: take(&mut node.obj),
+        }));
+        node.prop = swc_ecma_ast::MemberProp::Computed(ComputedPropName {
+            span: node.span,
+            expr: Box::new(Expr::Call(CallExpr {
+                span: node.span,
+                ctxt: Default::default(),
+                callee: swc_ecma_ast::Callee::Expr(Box::new(Expr::Member(MemberExpr {
+                    span: node.span,
+                    obj: Box::new(self.core.clone()),
+                    prop: swc_ecma_ast::MemberProp::Ident(IdentName {
+                        span: node.span,
+                        sym: Atom::new("$member"),
+                    }),
+                }))),
+                args: [
+                    ExprOrSpread {
+                        spread: None,
+                        expr: camobj.into(),
+                    },
+                    ExprOrSpread {
+                        expr: match take(&mut node.prop) {
+                            swc_ecma_ast::MemberProp::Ident(ident_name) => {
+                                Box::new(Expr::Lit(Lit::Str(Str {
+                                    span: ident_name.span,
+                                    value: ident_name.sym,
+                                    raw: None,
+                                })))
+                            }
+                            swc_ecma_ast::MemberProp::PrivateName(private_name) => todo!(),
+                            swc_ecma_ast::MemberProp::Computed(computed_prop_name) => {
+                                computed_prop_name.expr
+                            }
+                        },
+                        spread: None,
+                    },
+                    ExprOrSpread {
+                        spread: None,
+                        expr: Box::new(Expr::Lit(Lit::Str(Str {
+                            span: node.span,
+                            value: Atom::new(&*self.cfg.name),
+                            raw: None,
+                        }))),
+                    },
+                ]
+                .into_iter()
+                .collect(),
+                type_args: None,
+            })),
+        })
+    }
+    fn visit_mut_expr(&mut self, node: &mut Expr) {
+        if let Some(o) = node.as_mut_object() {
+            o.props = o
+                .props
+                .drain(..)
+                .map(|a| match a {
+                    PropOrSpread::Prop(p) => match *p {
+                        Prop::Shorthand(s) => {
+                            PropOrSpread::Prop(Box::new(Prop::KeyValue(KeyValueProp {
+                                key: s.clone().into(),
+                                value: s.into(),
+                            })))
+                        }
+                        p => PropOrSpread::Prop(Box::new(p)),
+                    },
+                    a => a,
+                })
+                .collect()
+        }
+        node.visit_mut_children_with(self);
+        *node = match take(node) {
+            Expr::Bin(BinExpr {
+                left,
+                right,
+                span,
+                op: BinaryOp::In,
+            }) if !left.is_private_name() => {
+                let camobj =
+                    Ident::new_private(Atom::new(format!("{}$camobj", &self.cfg.name)), span);
+                let cammem =
+                    Ident::new_private(Atom::new(format!("{}$cammem", &self.cfg.name)), span);
+                self.decls.insert(camobj.to_id());
+                self.decls.insert(cammem.to_id());
+                Expr::Seq(SeqExpr {
+                    span,
+                    exprs: [
+                        Box::new(Expr::Assign(AssignExpr {
+                            span,
+                            op: AssignOp::Assign,
+                            left: cammem.clone().into(),
+                            right: left,
+                        })),
+                        Box::new(Expr::Assign(AssignExpr {
+                            span,
+                            op: AssignOp::Assign,
+                            left: camobj.clone().into(),
+                            right: right,
+                        })),
+                        Box::new(Expr::Bin(BinExpr {
+                            span,
+                            op: BinaryOp::In,
+                            left: Box::new(Expr::Call(CallExpr {
+                                span: span,
+                                ctxt: Default::default(),
+                                callee: swc_ecma_ast::Callee::Expr(Box::new(Expr::Member(
+                                    MemberExpr {
+                                        span: span,
+                                        obj: Box::new(self.core.clone()),
+                                        prop: swc_ecma_ast::MemberProp::Ident(IdentName {
+                                            span: span,
+                                            sym: Atom::new("$member"),
+                                        }),
+                                    },
+                                ))),
+                                args: [
+                                    ExprOrSpread {
+                                        spread: None,
+                                        expr: camobj.clone().into(),
+                                    },
+                                    ExprOrSpread {
+                                        expr: cammem.into(),
+                                        spread: None,
+                                    },
+                                    ExprOrSpread {
+                                        spread: None,
+                                        expr: Box::new(Expr::Lit(Lit::Str(Str {
+                                            span: span,
+                                            value: Atom::new(&*self.cfg.name),
+                                            raw: None,
+                                        }))),
+                                    },
+                                ]
+                                .into_iter()
+                                .collect(),
+                                type_args: None,
+                            })),
+                            right: camobj.into(),
+                        })),
+                    ]
+                    .into_iter()
+                    .collect(),
+                })
+            }
+            node => node,
+        }
+    }
+    fn visit_mut_ident(&mut self, node: &mut Ident) {
+        node.visit_mut_children_with(self);
+        if self.cfg.hookable.contains(&node.sym) {
+            node.sym = Atom::new(format!("{}$user${}", &self.cfg.name, &node.sym))
+        }
     }
 }
